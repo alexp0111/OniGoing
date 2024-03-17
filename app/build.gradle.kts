5948caf0ab6cadd68e6ea73c957a38f362e16877
Alexander <alexp0111@mail.ru>
2024-03-17 19:36:40 +0300

    Basic libraries set up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,11 +37,36 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    /** Base core libraries */
+    implementation(localDeps.kotlin)
+    implementation(localDeps.lifecycleRuntime)
+    implementation(localDeps.lifecycleViewModel)
+    implementation(localDeps.appCompat)
+    implementation(localDeps.materialDesign)
+    implementation(localDeps.constraintLayout)
+    implementation(localDeps.swipeRefreshLayout)
+
+    /** Dagger2 */
+    implementation(localDeps.dagger)
+    kapt(localDeps.compiler)
+
+    /** Glide */
+    implementation(localDeps.glide)
+
+    /** Cicerone */
+    implementation(localDeps.cicerone)
+
+    /** Gson */
+    implementation(localDeps.gson)
+
+    /** Unit testing */
+    testImplementation(unitTestDeps.jUnit)
+    testImplementation(unitTestDeps.robolectric)
+    testImplementation(unitTestDeps.mockito)
+
+    /** Instrumented testing */
+    androidTestImplementation(uiTestDeps.extJUnit)
+    androidTestImplementation(uiTestDeps.espresso)
+    androidTestImplementation(uiTestDeps.rules)
+    androidTestImplementation(uiTestDeps.runner)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,39 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+
+    versionCatalogs {
+        create("localDeps") {
+            library("kotlin", "androidx.core:core-ktx:1.12.0")
+            library("lifecycleRuntime", "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+            library("lifecycleViewModel", "androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+            library("appCompat", "androidx.appcompat:appcompat:1.6.1")
+            library("materialDesign", "com.google.android.material:material:1.11.0")
+            library("constraintLayout", "androidx.constraintlayout:constraintlayout:2.1.4")
+            library("swipeRefreshLayout", "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+            library("dagger", "com.google.dagger:dagger:2.51")
+            library("compiler", "com.google.dagger:dagger-compiler:2.51")
+
+            library("glide", "com.github.bumptech.glide:glide:3.7.0")
+            library("gson", "com.google.code.gson:gson:2.10.1")
+            library("cicerone", "com.github.terrakok:cicerone:7.1")
+        }
+        create("unitTestDeps") {
+            library("jUnit", "junit:junit:4.13.2")
+            library("robolectric", "org.robolectric:robolectric:4.4")
+            library("mockito", "org.mockito:mockito-core:5.1.1")
+        }
+        create("uiTestDeps") {
+            library("extJUnit", "androidx.test.ext:junit:1.1.5")
+            library("espresso", "androidx.test.espresso:espresso-core:3.5.1")
+            library("rules", "androidx.test:rules:1.5.0")
+            library("runner", "androidx.test:runner:1.5.2")
+        }
     }
 }
 
